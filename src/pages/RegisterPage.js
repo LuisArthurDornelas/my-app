32602864561f5ec12a4ae4c626e13a254aefeaba
lui <ladccc@cesar.school>
2024-06-26 01:05:26 -0300

    adjusts api call+validations

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
-import axios from 'axios';
+import api from '../services/api';
+import Header from '../components/Header';
 import './RegisterPage.css';
 
-function Cadastro() {
+const RegisterPage = () => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const [confirmarSenha, setConfirmarSenha] = useState('');
@@ -12,11 +13,13 @@ function Cadastro() {
     const [cpf, setCpf] = useState('');
     const [dataNascimento, setDataNascimento] = useState('');
     const [telefone, setTelefone] = useState('');
-    const [estadoCivil, setEstadoCivil] = useState('Solteiro');
-    const [escolaridade, setEscolaridade] = useState('2o grau completo');
+    const [estadoCivil, setEstadoCivil] = useState('solteiro(a)');
+    const [escolaridade, setEscolaridade] = useState('2grau_completo');
     const navigate = useNavigate();
 
-    const handleCadastro = () => {
+    const handleCadastro = async (e) => {
+        e.preventDefault();
+
         if (!email || !validateEmail(email)) {
             alert('Por favor, preencha o e-mail com um e-mail válido.');
             return;
@@ -41,15 +44,28 @@ function Cadastro() {
             alert('Você deve ter pelo menos 18 anos.');
             return;
         }
-        axios.post('/cadastro', { email, senha, nome, cpf, dataNascimento, telefone, estadoCivil, escolaridade })
-            .then(response => {
-                if (response.data.status === 'success') {
-                    alert('Cadastro realizado com sucesso');
-                    navigate('/login');
-                } else {
-                    alert(response.data.message);
-                }
+
+        try {
+            const response = await api.post('/clients', {
+                email,
+                senha,
+                nome,
+                cpf,
+                dataNascimento,
+                telefone,
+                estadoCivil,
+                escolaridade
             });
+            if (response.data.status === 'success') {
+                alert('Cadastro realizado com sucesso');
+                navigate('/login');
+            } else {
+                alert('Erro no cadastro');
+            }
+        } catch (error) {
+            console.error('Error registering client:', error);
+            alert('Erro ao tentar cadastrar');
+        }
     };
 
     const handleClear = () => {
@@ -60,8 +76,8 @@ function Cadastro() {
         setCpf('');
         setDataNascimento('');
         setTelefone('');
-        setEstadoCivil('Solteiro');
-        setEscolaridade('2o grau completo');
+        setEstadoCivil('solteiro(a)');
+        setEscolaridade('2grau_completo');
         document.getElementById('email').focus();
     };
 
@@ -86,7 +102,7 @@ function Cadastro() {
                 </button>
             </header>
             <main>
-                <form className="cadastro-form">
+                <form className="cadastro-form" onSubmit={handleCadastro}>
                     <label htmlFor="email">E-mail (Login):</label>
                     <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required />
                     <label htmlFor="senha">Senha:</label>
@@ -94,7 +110,7 @@ function Cadastro() {
                     <label htmlFor="confirmarSenha">Confirmar Senha:</label>
                     <input type="password" id="confirmarSenha" name="confirmarSenha" value={confirmarSenha} onChange={e => setConfirmarSenha(e.target.value)} required />
                     <p>
-                        Regras de senha: Mínimo de 6 caracteres, incluindo pelo menos uma letra maiúscula, um número e um caractere especial (@ # $ % & * ! ? / \ | - _ + . =).<br />
+                        Regras de senha: Mínimo de 6 caracteres, incluindo pelo menos uma letra maiúscula, um número e um caractere especial (@ # $ % & * ! ? / \\ | - _ + . =).<br />
                         Caracteres não permitidos: ̈ { } [ ]  ́ ` ~ ^ : ; &lt; &gt; , “ ‘
                     </p>
                     <label htmlFor="nome">Nome:</label>
@@ -107,21 +123,21 @@ function Cadastro() {
                     <input type="tel" id="telefone" name="telefone" value={telefone} onChange={e => setTelefone(e.target.value)} />
                     <label htmlFor="estadoCivil">Estado Civil:</label>
                     <div className="radio-group">
-                        <label><input type="radio" id="solteiro" name="estadoCivil" value="Solteiro" checked={estadoCivil === 'Solteiro'} onChange={e => setEstadoCivil(e.target.value)} /> Solteiro</label>
-                        <label><input type="radio" id="casado" name="estadoCivil" value="Casado" checked={estadoCivil === 'Casado'} onChange={e => setEstadoCivil(e.target.value)} /> Casado</label>
-                        <label><input type="radio" id="divorciado" name="estadoCivil" value="Divorciado" checked={estadoCivil === 'Divorciado'} onChange={e => setEstadoCivil(e.target.value)} /> Divorciado</label>
-                        <label><input type="radio" id="viuvo" name="estadoCivil" value="Viuvo" checked={estadoCivil === 'Viuvo'} onChange={e => setEstadoCivil(e.target.value)} /> Viúvo</label>
+                        <label><input type="radio" id="solteiro" name="estadoCivil" value="solteiro(a)" checked={estadoCivil === 'solteiro(a)'} onChange={e => setEstadoCivil(e.target.value)} /> Solteiro</label>
+                        <label><input type="radio" id="casado" name="estadoCivil" value="casado(a)" checked={estadoCivil === 'casado(a)'} onChange={e => setEstadoCivil(e.target.value)} /> Casado</label>
+                        <label><input type="radio" id="divorciado" name="estadoCivil" value="divorciado(a)" checked={estadoCivil === 'divorciado(a)'} onChange={e => setEstadoCivil(e.target.value)} /> Divorciado</label>
+                        <label><input type="radio" id="viuvo" name="estadoCivil" value="viuvo(a)" checked={estadoCivil === 'viuvo(a)'} onChange={e => setEstadoCivil(e.target.value)} /> Viúvo</label>
                     </div>
                     <label htmlFor="escolaridade">Escolaridade:</label>
                     <select id="escolaridade" name="escolaridade" value={escolaridade} onChange={e => setEscolaridade(e.target.value)}>
-                        <option value="1o grau incompleto">1o grau incompleto</option>
-                        <option value="1o grau completo">1o grau completo</option>
-                        <option value="2o grau completo">2o grau completo</option>
-                        <option value="nível superior">Nível Superior</option>
-                        <option value="pós-graduado">Pós-graduado</option>
+                        <option value="1grau_incompleto">1º grau incompleto</option>
+                        <option value="1grau_completo">1º grau completo</option>
+                        <option value="2grau_completo">2º grau completo</option>
+                        <option value="nivel_superior">Nível superior</option>
+                        <option value="pos_graduado">Pós-graduado</option>
                     </select>
                     <div className="button-group">
-                        <button type="button" onClick={handleCadastro}>Incluir</button>
+                        <button type="submit">Incluir</button>
                         <button type="button" onClick={handleClear}>Limpar</button>
                         <button type="button" onClick={() => navigate('/login')}>Voltar</button>
                     </div>
@@ -129,7 +145,7 @@ function Cadastro() {
             </main>
         </div>
     );
-}
+};
 
 function validateEmail(email) {
     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -157,4 +173,4 @@ function validateCPF(cpf) {
     return resto === parseInt(cpf.substring(10, 11));
 }
 
-export default Cadastro;
+export default RegisterPage;
